f975ae0d72e0ff22f5d9cf993c0b4a6ad73d9094
tejasvik-5 <tkandpal007@gmail.com>
2026-02-17 14:50:46 +0530

    Added ques17.c

---
+++ ques17.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+    int n;
+    // Read size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Assume first element is both max and min
+    int max = arr[0];
+    int min = arr[0];
+
+    // Traverse array
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    // Output
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+    return 0;
+}
