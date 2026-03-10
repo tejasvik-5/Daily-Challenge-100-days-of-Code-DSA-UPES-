90720de3491de7cb5326cd677b37fbc0fc0237a0
tejasvik-5 <tkandpal007@gmail.com>
2026-03-10 10:44:59 +0530

    added ques38.c

---
+++ ques38.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#define MAX 100
+
+int deque[MAX];
+int front = -1, rear = -1;
+
+int isEmpty() {
+    return (front == -1);
+}
+
+int isFull() {
+    return ((rear + 1) % MAX == front);
+}
+
+void push_front(int x) {
+    if(isFull()) {
+        printf("Deque Overflow\n");
+        return;
+    }
+
+    if(isEmpty()) {
+        front = rear = 0;
+    }
+    else if(front == 0) {
+        front = MAX - 1;
+    }
+    else {
+        front--;
+    }
+
+    deque[front] = x;
+}
+
+void push_back(int x) {
+    if(isFull()) {
+        printf("Deque Overflow\n");
+        return;
+    }
+
+    if(isEmpty()) {
+        front = rear = 0;
+    }
+    else if(rear == MAX - 1) {
+        rear = 0;
+    }
+    else {
+        rear++;
+    }
+
+    deque[rear] = x;
+}
+
+void pop_front() {
+    if(isEmpty()) {
+        printf("Deque Underflow\n");
+        return;
+    }
+
+    if(front == rear) {
+        front = rear = -1;
+    }
+    else if(front == MAX - 1) {
+        front = 0;
+    }
+    else {
+        front++;
+    }
+}
+
+void pop_back() {
+    if(isEmpty()) {
+        printf("Deque Underflow\n");
+        return;
+    }
+
+    if(front == rear) {
+        front = rear = -1;
+    }
+    else if(rear == 0) {
+        rear = MAX - 1;
+    }
+    else {
+        rear--;
+    }
+}
+
+int getFront() {
+    if(isEmpty()) return -1;
+    return deque[front];
+}
+
+int getBack() {
+    if(isEmpty()) return -1;
+    return deque[rear];
+}
+
+int size() {
+    if(isEmpty()) return 0;
+    if(rear >= front)
+        return rear - front + 1;
+    else
+        return MAX - front + rear + 1;
+}
+
+void display() {
+    if(isEmpty()) {
+        printf("Deque is empty\n");
+        return;
+    }
+
+    int i = front;
+    while(1) {
+        printf("%d ", deque[i]);
+        if(i == rear) break;
+        i = (i + 1) % MAX;
+    }
+    printf("\n");
+}
+
+int main() {
+
+    push_back(10);
+    push_back(20);
+    push_front(5);
+    push_front(2);
+
+    display();
+
+    printf("Front: %d\n", getFront());
+    printf("Back: %d\n", getBack());
+    printf("Size: %d\n", size());
+
+    pop_front();
+    pop_back();
+
+    display();
+
+    return 0;
+}
