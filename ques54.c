95f120b785c373879b80f61971b7e063c5fad630
tejasvik-5 <tkandpal007@gmail.com>
2026-03-26 07:13:16 +0530

    added ques54.c

---
+++ ques54.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int val) {
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    struct Node* root = newNode(arr[0]);
+
+    struct Node* queue[2000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void zigzagTraversal(struct Node* root) {
+    if (!root) return;
+
+    struct Node* queue[2000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    int leftToRight = 1;
+
+    while (front < rear) {
+        int size = rear - front;
+        int level[size];
+
+        for (int i = 0; i < size; i++) {
+            struct Node* node = queue[front++];
+
+            int index = leftToRight ? i : (size - 1 - i);
+            level[index] = node->data;
+
+            if (node->left) queue[rear++] = node->left;
+            if (node->right) queue[rear++] = node->right;
+        }
+
+        for (int i = 0; i < size; i++) {
+            printf("%d ", level[i]);
+        }
+
+        leftToRight = !leftToRight;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    struct Node* root = buildTree(arr, n);
+    zigzagTraversal(root);
+
+    return 0;
+}
