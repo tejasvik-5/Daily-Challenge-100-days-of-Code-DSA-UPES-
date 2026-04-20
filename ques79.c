486884b9d4156981d7452c0c5e65199b79e3afb8
tejasvik-5 <tkandpal007@gmail.com>
2026-04-20 13:33:13 +0530

    added ques79.c

---
+++ ques79.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+int minDistance(int dist[], int visited[], int n) {
+    int min = INT_MAX, min_index;
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i] && dist[i] <= min) {
+            min = dist[i];
+            min_index = i;
+        }
+    }
+    return min_index;
+}
+
+void dijkstra(int n, int graph[MAX][MAX], int src) {
+    int dist[MAX], visited[MAX];
+
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = 0;
+    }
+
+    dist[src] = 0;
+
+    for (int count = 1; count <= n - 1; count++) {
+        int u = minDistance(dist, visited, n);
+        visited[u] = 1;
+
+        for (int v = 1; v <= n; v++) {
+            if (!visited[v] && graph[u][v] &&
+                dist[u] != INT_MAX &&
+                dist[u] + graph[u][v] < dist[v]) {
+                
+                dist[v] = dist[u] + graph[u][v];
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", dist[i]);
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int graph[MAX][MAX] = {0};
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        graph[u][v] = w;
+       
+    }
+
+    int source;
+    scanf("%d", &source);
+
+    dijkstra(n, graph, source);
+
+    return 0;
+}
